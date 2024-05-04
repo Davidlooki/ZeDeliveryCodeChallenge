94209c30653d6516bacdf0c51e9e520e9b229d0d
Davidlooki <davidlooki@hotmail.com>
2024-05-04 00:25:21 -0300

    Created helper methods to convert Geometry data.

--- ZeDeliveryCodeChallenge/Controller/PartnersController.cs
+++ ZeDeliveryCodeChallenge/Controller/PartnersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
 
 namespace ZeDeliveryCodeChallenge
 {
@@ -9,7 +11,13 @@ namespace ZeDeliveryCodeChallenge
         [HttpGet]
         public async Task<IActionResult> GetPartners()
         {
-            try { return Ok(await partnerRepository.GetPartners()); }
+            try
+            {
+                var partners = await partnerRepository.GetPartners();
+                var geoJSONFeatureCollection = ConvertToGeoJSON(partners);
+
+                return Ok(partners);
+            }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError,
@@ -42,7 +50,7 @@ namespace ZeDeliveryCodeChallenge
 
                 var checkPartnerId = await partnerRepository.GetPartnerById(partner.id);
 
-                if (checkPartnerId != null)
+                if (checkPartnerId is not null)
                 {
                     ModelState.AddModelError("id", "Partner id already in use.");
 
@@ -51,7 +59,7 @@ namespace ZeDeliveryCodeChallenge
 
                 var checkPartnerDocument = await partnerRepository.GetPartnerByDocument(partner.document);
 
-                if (checkPartnerDocument != null)
+                if (checkPartnerDocument is not null)
                 {
                     ModelState.AddModelError("document", "Partner document already in use.");
 
@@ -59,15 +67,57 @@ namespace ZeDeliveryCodeChallenge
                 }
 
                 var createdPartner = await partnerRepository.CreatePartner(partner);
+                var geoJSONFeatureCollection = ConvertToGeoJSON(new List<Partner>
+                { createdPartner });
 
-                return CreatedAtAction(nameof(GetPartnerById), new
-                { id = createdPartner.id }, createdPartner);
+                return Ok(geoJSONFeatureCollection);
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     "Error creating new partner record");
             }
+        }
+
+        private static GeoJSON.Net.Geometry.MultiPolygon ConvertGeometryToGeoJSON(
+            NetTopologySuite.Geometries.MultiPolygon ntsMultiPolygon
+        )
+        {
+            var geoJSONMultiPolygon = new GeoJSON.Net.Geometry.MultiPolygon(
+                ntsMultiPolygon.Geometries.Select(ntsPolygon =>
+                    new Polygon(
+                        new List<LineString>
+                        { new LineString(
+                              ntsPolygon.Coordinates.Select(ntsCoordinate =>
+                                  new Position(ntsCoordinate.Y, ntsCoordinate.X)).ToList()
+                          ) }
+                    )).ToList()
+            );
+
+            return geoJSONMultiPolygon;
+        }
+
+        private static GeoJSON.Net.Feature.FeatureCollection ConvertToGeoJSON(IEnumerable<Partner> partners)
+        {
+            var featureCollection = new GeoJSON.Net.Feature.FeatureCollection();
+
+            foreach (var partner in partners)
+            {
+                var properties = new Dictionary<string, object>
+                { { "id", partner.id },
+                  { "tradingName", partner.tradingName },
+                  { "ownerName", partner.ownerName },
+                  { "document", partner.document },
+                  { "coverageArea", partner.coverageArea },
+                  { "address", partner.address } };
+
+                var geometry = ConvertGeometryToGeoJSON(partner.coverageArea);
+
+                var feature = new Feature(geometry, properties);
+                featureCollection.Features.Add(feature);
+            }
+
+            return featureCollection;
         }
     }
 }
