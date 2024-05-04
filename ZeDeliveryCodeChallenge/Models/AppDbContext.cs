c041d3b13e52ad0a9f6558a25b6b961e0247db74
Davidlooki <davidlooki@hotmail.com>
2024-05-04 00:16:00 -0300

    Apply postgres server connection configurations.

--- ZeDeliveryCodeChallenge/Models/AppDbContext.cs
+++ ZeDeliveryCodeChallenge/Models/AppDbContext.cs
@@ -4,7 +4,13 @@ namespace ZeDeliveryCodeChallenge
 {
     public class AppDbContext : DbContext, IPartnerDbSet
     {
-        public AppDbContext() { } //Migration requires an empty constructor to work.
         public DbSet<Partner> Partners { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=admin;",
+                pjr => pjr.UseNetTopologySuite());
+        }
     }
 }
