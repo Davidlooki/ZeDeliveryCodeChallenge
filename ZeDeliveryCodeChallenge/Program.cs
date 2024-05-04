09c312f369f579849fda7fe2748450f4d656180b
Davidlooki <davidlooki@hotmail.com>
2024-05-04 00:32:02 -0300

    Added service for convert Geometry to JSON

--- ZeDeliveryCodeChallenge/Program.cs
+++ ZeDeliveryCodeChallenge/Program.cs
@@ -1,8 +1,18 @@
+using NetTopologySuite;
+using NetTopologySuite.IO.Converters;
 using ZeDeliveryCodeChallenge;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+    options.SerializerSettings.Converters.Add(new GeometryConverter(geometryFactory));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
